65cffcdd1b22b89125b1d7039be3441833189c24
soasme <soasme@gmail.com>
2018-03-31 22:46:33 +1300

    disallow other users.

--- hana/settings.py
+++ hana/settings.py
@@ -8,3 +8,4 @@ OAUTH2_REFRESH_TOKEN_GENERATOR = 'hana.auth.app.generate_token'
 GITHUB_CLIENT_ID = os.environ['GITHUB_CLIENT_ID']
 GITHUB_CLIENT_SECRET = os.environ['GITHUB_CLIENT_SECRET']
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.environ['SQLALCHEMY_DATABASE_URI']
+ALLOWED_USERS = [u for u in os.environ.get('HANA_ALLOWED_USERS').split() if u]
